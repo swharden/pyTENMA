9058becb608fd1c0c5156f1ea93e856214ccc5d8
Scott W Harden <swharden@gmail.com>
2016-08-23 00:12:21 -0400

    Update logPlot.py

--- extras/logPlot.py
+++ extras/logPlot.py
@@ -1,15 +1,15 @@
 """quick and dirty demo how to plot a text file log."""
 import pylab
 import numpy as np
-import datetime
+#import datetime
 if __name__=="__main__":
     with open("log.txt") as f:
         raw=f.read()
     raw=raw.split("\n#")[-1] # select the last log session
     raw=raw.strip().split("\n") # make it multiline (easy for data conversion)
     timestamp,data=raw[0],np.array(raw[1:]).astype(np.float) # data is now np
-    timestamp=int(timestamp.strip().split(" ")[1]) # epoch seconds
-    start=datetime.datetime.fromtimestamp(timestamp) # datetime object
+    #timestamp=int(timestamp.strip().split(" ")[1]) # epoch seconds
+    #start=datetime.datetime.fromtimestamp(timestamp) # datetime object
     
     # we have our data as a numpy array ready for plotting
     # we have the start time we could use for the X axis if we wanted to
